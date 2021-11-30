eff9761da85193d6536d3450e5f57bea05a0c7f0
HarshCasper <erbeusgriffincasper@gmail.com>
2021-11-30 09:49:19 +0530

    CHORE: Remove auto-generated files from Docusaurus config

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/nick/enarx-docs/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/nick/enarx-docs/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/nick/enarx-docs/docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/home/nick/enarx-docs/docs/src/css/custom.css"),
-];
